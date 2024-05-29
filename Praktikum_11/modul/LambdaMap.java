8dd0b33c87f4723c4a39f9c2bbb67b9d81852d6f
Hamam Azidani <hmm.zid1009@gmail.com>
2024-05-29 10:55:38 +0700

    modul praktikum 11

---
+++ Praktikum_11/modul/LambdaMap.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * File: LambdaMap.java
+ * Deskripsi: Implementasi lambda pada Map, digunakan untuk menampilkan key dan value.
+ */
+
+public class LambdaMap {
+    public static void main(String[] args) {
+        // Membuat Map dengan key NIM dan value nama mahasiswa
+        Map<String, String> mahasiswaMap = new HashMap<>();
+        mahasiswaMap.put("24060122130099", "Hamam Azidani");
+        mahasiswaMap.put("24060122130079", "Sultan Alamsyah Borneo Arifin");
+        mahasiswaMap.put("24060122130059", "Arifin Nurmuhammad Haris");
+        mahasiswaMap.put("24060122130083", "Muhammad Ahsan Yudhistira");
+
+        // Menggunakan lambda untuk menampilkan key dan value
+        mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " + nim + ", Nama: " + nama));
+    }
+}
